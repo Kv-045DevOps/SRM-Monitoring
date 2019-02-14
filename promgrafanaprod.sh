53ed020d1c16f7626ab16b2f12897857bf8a03c1
VitaliiS-983 <42441367+VitaliiS-983@users.noreply.github.com>
2019-02-14 11:15:38 +0200

    Add files via upload

--- promgrafanaprod.sh
+++ promgrafanaprod.sh
@@ -2,6 +2,6 @@ helm repo update;
 sleep 5;
 helm install --namespace monitoring --name prometheus stable/prometheus -f promethconf.yaml;
 sleep 5;
-kubectl create -f kubistiodash.yaml;
-helm install --namespace production --name grafana stable/grafana --set adminUser=admin --set adminPassword=admin123 --set sidecar.dashboards.enabled=true --set sidecar.datasources.enabled=true
+kubectl create -f kubistiodash1.yaml;
+helm install --namespace monitoring --name grafana stable/grafana --set adminUser=admin --set adminPassword=admin123 --set sidecar.dashboards.enabled=true --set sidecar.datasources.enabled=true
 kubectl get pods -n monitoring
